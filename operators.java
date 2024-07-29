7917f9bae1182fd69f7de71db6413b4d2439ccb6
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-07-30 01:51:01 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,9 @@
+public class operators {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int add = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + add);
+        System.out.println(num1 < 2 && num2 > 1);
+    }
+}

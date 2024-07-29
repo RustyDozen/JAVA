150eeb1e2ede19c92a5cd2964c54996cc6504be2
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-07-30 01:39:18 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int myInt1 = 1;
+        double myDouble1 = myInt1;
+        System.out.println(myDouble1);
+        double myDouble2 = 1.2d;
+        int myInt2 = (int) myDouble2;
+        System.out.println(myInt2);
+        int maxScore = 500;
+        int userScore = 423;
+        float perc = (float) userScore / maxScore * 100.0f;
+        System.out.println("User's percentage is " + perc + "%");
+    }
+}

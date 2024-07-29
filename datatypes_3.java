9f1c6ee23727637d325f1f984a787fa3cfb9d1e2
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-07-29 23:26:03 +0530

    Add files via upload

---
+++ datatypes_3.java
@@ -0,0 +1,14 @@
+public class datatypes_3 {
+    public static void main(String[] args) {
+        int myNum = 1; //int - -2147483648 to 2147483647
+        float myFloat = 1.1f;
+        char myChar = 'Y';
+        boolean myBool = true;
+        String myString = "Yeaman";
+        byte num1 = 2; //byte - -128 to 127
+        short num2 = 3; //short - -32768 to 32767
+        long num3 = 4; //long - -9223372036854775808 to 9223372036854775807
+        float scientificNum1 = 32e3f;
+        double scientificNum2 = 12E4d;
+    }
+}

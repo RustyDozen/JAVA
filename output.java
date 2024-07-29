f3f5dee4cde8b7dff97c4df629965da433b73672
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-07-29 22:46:35 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,9 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.println("println() adds a new line");
+        System.out.print("print() doesn't add new line");
+        System.out.println();
+        System.out.println(1);
+        System.out.println(1 * 2);
+    }
+}

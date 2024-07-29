0cb66cb22d199d4b4d17e52596e60960103f43e7
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-07-29 22:47:58 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables{
+    public static void main(String[] args){
+        int myNum = 1;
+        float myFloatNum = 1.1f;
+        char myChar = 'c';
+        boolean myBoolean = true;
+        String myString = "string";
+        String firstName = "Sk";
+        String lastName = "Yeaman";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        int num1 = 2;
+        int num2 = 3;
+        int add = num1 + num2;
+        System.out.println("Add of " + num1 + " and " + num2 + " is " + add);
+    }
+}

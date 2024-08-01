39a3c785ea799192955bb4beb650fc3ef62a4684
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-02 01:27:09 +0530

    Add files via upload

---
+++ Tutorial/arrays.java
@@ -0,0 +1,38 @@
+
+public class arrays {
+
+    public static void main(String[] args) {
+        /*String[] cars = {"AE86", "RX7", "R33"};
+        cars[2] = "R34";
+        System.out.println(cars[2]);
+        System.out.println("Array length: " + cars.length);
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        for (String i : cars) {
+            System.out.println(i);
+        }
+        int[] ages = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        float sum = 0, avg;
+        int length = ages.length;
+        for (int i : ages) {
+            sum += i;
+        }
+        avg = sum / length;
+        System.out.println("Average: " + avg);*/
+        int[] ages = {24, 12, 15, 18, 20, 19, 10};
+        int lowestAge = ages[0];
+        /*for (int age : ages) {
+            if (lowestAge > age) {
+                lowestAge = age;
+            }
+        }
+        System.out.println("The lowest age in the array: " + lowestAge);*/
+        for (int age = 0; age < ages.length; age++) {
+            if (lowestAge > ages[age]) {
+                lowestAge = ages[age];
+            }
+        }
+        System.out.println("The lowest age in the array: " + lowestAge);
+    }
+}

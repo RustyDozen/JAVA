a725fe3d5cc38fffa052508ae858ed037a51d1b6
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-01 16:56:44 +0530

    Add files via upload

---
+++ Tutorial/booleans.java
@@ -0,0 +1,17 @@
+
+public class booleans {
+
+    public static void main(String[] args) {
+        boolean amIgay = false;
+        boolean amIstraight = true;
+        int myAge = 18;
+        int votingAge = 18;
+        System.out.println("Am I gay: " + amIgay);
+        System.out.println("Am I straight: " + amIstraight);
+        if (myAge == votingAge) {
+            System.out.println("I am eligible to vote");
+        } else {
+            System.out.println("Not old enough");
+        }
+    }
+}

fc630724c6f7a0ed353476fcd38a01a92037979a
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-01 18:31:17 +0530

    Add files via upload

---
+++ Tutorial/whileloop.java
@@ -0,0 +1,30 @@
+
+public class whileloop {
+
+    public static void main(String[] args) {
+        /*int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Not Happy Birthday To Me");*/
+        int dice = 6;
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("Not Yatzy");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice++;
+        }
+    }
+}

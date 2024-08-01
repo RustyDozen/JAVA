7675beb27ffae1a51c839c5339785502dc5cf972
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-01 18:46:26 +0530

    Add files via upload

---
+++ Tutorial/forloop.java
@@ -0,0 +1,23 @@
+
+public class forloop {
+
+    public static void main(String[] args) {
+        /*for (int n = 0; n < 5; n++) {
+            System.out.println(n);
+        }
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("Outer loop: " + i);
+            for (int j = 0; j <= 3; j++) {
+                System.out.println("Inner loop: " + j);
+            }
+        }
+        String[] cars = {"AE86", "Rx7", "R33"};
+        for (String i : cars) {
+            System.out.println(i);
+        }*/
+        int num = 2;
+        for (int i = 1; i <=10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}

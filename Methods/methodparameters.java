3b87127a6caab7b03d15aec0e2e6830a751fd935
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-02 15:17:57 +0530

    Create methodparameters.java

---
+++ Methods/methodparameters.java
@@ -0,0 +1,21 @@
+public class methodparameters {
+    static void myMethod(String fname, String lname) {
+        System.out.println("Hello " + fname + " " + lname);
+    }
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("You're not old enough");
+        } else {
+            System.out.println("You're old enough to drink");
+        }
+    }
+    static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        int num1 = 1, num2 = 2;
+        myMethod("Sk", "Yeaman");
+        checkAge(20);
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum(num1, num2));
+    }
+}

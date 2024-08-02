f35094b8e154882f9e6ff48301f42424c5507ec7
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-02 15:42:55 +0530

    Add files via upload

---
+++ Methods/scope.java
@@ -0,0 +1,12 @@
+public class scope {
+    public static void main(String[] args) {
+        // Code here CAN'T use x
+        {
+            // Code here CAN'T use x
+            int x = 9;
+            // Code here CAN use x
+            System.out.println(x);
+        } // The block ends here
+        // Code here CAN'T use x
+    }
+}

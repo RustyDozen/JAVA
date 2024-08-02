9872e99140534f91b0653e9dfb46d87b3fd2a97a
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-02 23:29:45 +0530

    Add files via upload

---
+++ Classes/Attributes.java
@@ -0,0 +1,12 @@
+public class Attributes {
+    int x = 9, age = 18;
+    String fname = "Sk ", lname = "Yeaman";
+    public static void main(String[] args) {
+        Attributes myObj1 = new Attributes();
+        Attributes myObj2 = new Attributes();
+        myObj2.x = 7;
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+        System.out.println("My name is " + myObj1.fname + myObj1.lname + " and I am " + myObj1.age + " years old");
+    }
+}

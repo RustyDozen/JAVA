fce3bb49934a58db43d68f7663b0b86a67b98931
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-03 15:44:35 +0530

    Add files via upload

---
+++ Classes/Classmethods1.java
@@ -0,0 +1,17 @@
+public class Classmethods1 {
+    static void myStaticMethod() {
+        System.out.println("Staic method");
+    }
+
+    public void myPublicMethod() {
+        System.out.println("Public method");
+    }
+
+    public void fullThrottle() {
+        System.out.println("Car is running at full throttle");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is " + maxSpeed);
+    }
+}
---
+++ Classes/Classmethods2.java
@@ -0,0 +1,10 @@
+class Classmethods2 {
+    public static void main(String[] args) {
+        Classmethods1 myObj = new Classmethods1();
+        myObj.myPublicMethod();
+        Classmethods1 myCar = new Classmethods1();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+
+}

208b87a19c254e21c12bb4c49e7544784c15640f
Yeaman <167001452+RustyDozen@users.noreply.github.com>
2024-08-03 19:20:44 +0530

    Add files via upload

---
+++ Classes/Encapsulation2.java
@@ -0,0 +1,7 @@
+public class Encapsulation2 {
+    public static void main(String[] args) {
+        Encapsulation1 myObj = new Encapsulation1();
+        myObj.setName("Yeaman");
+        System.out.println(myObj.getName());
+    }
+}
